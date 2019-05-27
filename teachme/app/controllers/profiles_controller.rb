2ddde50fb0c3da69ffcd49c9df519b20e5e92591
James Pharr <james.pharr@eretaildata.com>
2019-05-27 09:33:20 -0400

    eliminate JSON related code

--- teachme/app/controllers/profiles_controller.rb
+++ teachme/app/controllers/profiles_controller.rb
@@ -2,13 +2,11 @@ class ProfilesController < ApplicationController
   before_action :set_profile, only: [:show, :edit, :update, :destroy]
 
   # GET /profiles
-  # GET /profiles.json
   def index
     @profiles = Profile.all
   end
 
   # GET /profiles/1
-  # GET /profiles/1.json
   def show
   end
 
@@ -23,7 +21,6 @@ class ProfilesController < ApplicationController
   end
 
   # POST /profiles
-  # POST /profiles.json
   def create
     @profile = Profile.new(profile_params)
 
@@ -37,26 +34,21 @@ class ProfilesController < ApplicationController
   end
 
   # PATCH/PUT /profiles/1
-  # PATCH/PUT /profiles/1.json
   def update
     respond_to do |format|
       if @profile.update(profile_params)
         format.html { redirect_to @profile, notice: 'Profile was successfully updated.' }
-        format.json { render :show, status: :ok, location: @profile }
       else
         format.html { render :edit }
-        format.json { render json: @profile.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /profiles/1
-  # DELETE /profiles/1.json
   def destroy
     @profile.destroy
     respond_to do |format|
       format.html { redirect_to profiles_url, notice: 'Profile was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
--- teachme/app/controllers/users_controller.rb
+++ teachme/app/controllers/users_controller.rb
@@ -2,13 +2,11 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # GET /users
-  # GET /users.json
   def index
     @users = User.all
   end
 
   # GET /users/1
-  # GET /users/1.json
   def show
   end
 
@@ -22,17 +20,14 @@ class UsersController < ApplicationController
   end
 
   # POST /users
-  # POST /users.json
   def create
     @user = User.new(user_params)
 
     respond_to do |format|
       if @user.save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
