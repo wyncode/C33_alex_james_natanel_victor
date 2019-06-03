0ebfb8d95878fb9bf9016b66d8c43e5c52ac3c11
James Pharr <james.pharr@eretaildata.com>
2019-06-03 14:24:13 -0400

    fixed the navbar

--- teachme/config/routes.rb
+++ teachme/config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
   root 'sessions#new'
-  # resources :reviews
-  resources :lessons do 
+  resources :reviews
+  resources :lessons do
     resources :user_lessons, only: [:create]
-  end 
+  end
   resources :users
   resources :profiles
   resources :sessions
 
-  get 'hello_world', to: 'hello_world#index'
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
