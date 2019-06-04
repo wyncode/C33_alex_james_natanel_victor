c467248f0c811995d07fc59b43ed370b1ed6df34
James Pharr <james.pharr@eretaildata.com>
2019-06-04 16:44:28 -0400

    Added new lesson to user profile

--- teachme/config/routes.rb
+++ teachme/config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   root 'sessions#new'
-  resources :users, except: :index 
+  resources :users, except: :index
   resources :profiles do
-    resources :lessons, only: :create
+    resources :lessons, only: [:create, :new]
   end
 
   post '/book/:lesson_id', to: 'lessons#book_lesson', as: 'book_lesson' #book_lesson_path(lesson_id: ...)
-  resources :lessons, except: :create
+  resources :lessons, except: [:create, :new]
   resources :reviews
   resources :sessions, only: [:create]
   delete '/logout', to: 'sessions#destroy', as: 'logout'
