36dfd0cbe97d0226117c6ec1bc1a8c143c579a80
superstaro <victorbrown33@hotmail.com>
2019-06-04 15:11:00 -0400

    reviews component

--- teachme/app/javascript/bundles/Reviews/components/NewReview.jsx
+++ teachme/app/javascript/bundles/Reviews/components/NewReview.jsx
@@ -3,40 +3,13 @@ import React from "react";
 const NewReview = props => {
   return (
     <React.Fragment>
-      <header>
-        <nav className="navbar">
-          <div id="menuToggle">
-            <input type="checkbox" />
-            <span />
-            <span />
-            <span />
-            <ul id="menu">
-              <li>
-                <a href="index.html">Home</a>
-              </li>
-              <li>
-                <a href="student.html">Student Sign Up</a>
-              </li>
-              <li>
-                <a href="teacher.html">Teacher Sign Up</a>
-              </li>
-            </ul>
-          </div>
-          <div className="logout-icon">
-            <a href="#">
-              <i className="fas fa-sign-out-alt fa-2x" />
-            </a>
-          </div>
-        </nav>
-      </header>
       <h3 style={{ padding: "1rem" }}>New Review</h3>
       <form style={{ padding: "1rem" }}>
         <h2>Leave your comments below:</h2>
-        <fieldset>
-          <legend />
-          <textarea cols="100" rows="20" />
+        <fieldset className="form-group">
+          <textarea className="form-control" />
           <br />
-          <button type="submit" value="Submit">
+          <button type="submit" className="btn btn-primary btn-block ">
             Submit
           </button>
         </fieldset>
