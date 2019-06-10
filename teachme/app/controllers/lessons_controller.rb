1f183f679c409051a4b203c2c208107c42278088
James Pharr <james.pharr@eretaildata.com>
2019-06-10 15:45:42 -0400

    fixed cancel lessons

--- teachme/app/controllers/lessons_controller.rb
+++ teachme/app/controllers/lessons_controller.rb
@@ -65,7 +65,7 @@ class LessonsController < ApplicationController
   def delete_lesson
     @user = current_user
     lesson = Lesson.find(params[:lesson_id])
-    @user.lessons.destroy
+    @user.lessons.delete(lesson)
     redirect_to @user
   end
   private
